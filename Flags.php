8bfedc53a162d4ddb47c1670528ea6daf332f32b
Ryonosuke Fujii <whoz51@yahoo.co.jp>
2012-07-13 19:56:46 +0900

    fix

--- Flags.php
+++ Flags.php
@@ -1,1 +1,30 @@
-<?phpclass Flags {	/*MEMBER*/	private $flags = 0;	/*CNSTRUCTOR*/	public function __construct() {}	/*METHOD*/		public function on($position) {		if (!($this->flags & 1<<$position)) {			$this->flags ^= 1 << $position;		}	}		public function off($position) {		if (($this->flags & 1<<$position)) {			$this->flags ^= 1 << $position;		}	}		public function watch() {		echo strrev(sprintf('%b', $this->flags));	}		public function check($position=0) {		return $this->flags & 1<<$position;	}	}?>
+<?php
+
+class Flags {
+	//
+	private $flags = 0;
+	//
+	public function __construct() {}
+	
+	public function on($position) {
+		if (!($this->flags & 1<<$position)) {
+			$this->flags ^= 1 << $position;
+		}
+	}
+	
+	public function off($position) {
+		if (($this->flags & 1<<$position)) {
+			$this->flags ^= 1 << $position;
+		}
+	}
+	
+	public function watch() {
+		echo strrev(sprintf('%b', $this->flags));
+	}
+	
+	public function check($position=0) {
+		return $this->flags & 1<<$position;
+	}	
+}
+
+?>
